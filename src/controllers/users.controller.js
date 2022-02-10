4ab21e30f85df02526e3c71426edc38a1fed7fde
Jesús <jesusht87@gmail.com>
2022-02-10 20:15:54 +0000

    USERS - CRUD operations done and tested

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -17,15 +17,41 @@ const createUser = async (req, res) => {
         )
 
         res.json({
-            name: user.name, 
+            name: user.name,
             email: user.email
         })
 
     } catch (error) {
-        res.status(500).send('Error creating user.')
+        res.status(500).send(error)
     }
 }
 
+function getAllUsers(req, res) {
+    userModel.find().then(response => res.json(response)).catch((err) => handleError(err, res))
+}
+
+function updateUser(req, res) {
+    userModel
+        .findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        })
+        .then(response => res.json(response))
+        .catch((err) => handleError(err, res))
+}
+
+function deleteUserById (req, res) {
+    userModel
+      .remove({ _id: req.params.id })
+      .then(response => res.json(response))
+      .catch(err => handleError(err, res))
+  }
+
+
+
 module.exports = {
-    createUser
+    createUser,
+    getAllUsers,
+    updateUser, 
+    deleteUserById
 }
--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -1,8 +1,13 @@
 const router = require('express').Router()
 const {
-    createUser
+    createUser, 
+    getAllUsers,
+    updateUser,
+    deleteUserById
 } = require('../controllers/users.controller')
 
 router.post('/', createUser)
-
+router.get('/', getAllUsers)
+router.put('/:id', updateUser)
+router.delete('/:id', deleteUserById)
 module.exports = router 
