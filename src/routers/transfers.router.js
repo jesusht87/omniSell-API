c50737a102a602c163d4f0380d7259f11f35d225
Jesús <jesusht87@gmail.com>
2022-02-16 20:35:55 +0000

    TRANSFERS - Deliver and Receive functions finished

--- src/routers/transfers.router.js
+++ src/routers/transfers.router.js
@@ -1,12 +1,14 @@
 const router = require ('express').Router()
 const {getTransfers,
        getTransferById,
-       deliver} = require('../controllers/transfers.controller')
+       deliver,
+       receive} = require('../controllers/transfers.controller')
 
 
 router.get('/', getTransfers)
 router.get('/:id', getTransferById)
-router.put('/:id', deliver)
+router.put('/deliver/:id', deliver)
+router.put('/receive/:id', receive)
 
 
 module.exports = router
